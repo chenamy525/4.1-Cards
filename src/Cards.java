84e67f4445454adf42aa5c04ea33f189aa63ad13
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2018-03-13 09:18:44 -0400

    pp

---
+++ src/Cards.java
@@ -0,0 +1,17 @@
+
+public class Cards {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public Deck (String[] rank, String[] suit, int[] value) {
+		for (int i = 0; i< rank.length; i++) {
+			for (int j = 0; j < suit.length; j++) {
+				unDealt.add(new card(rank[i], suit[j], value[i]);
+			}
+		}
+	}
+
+}
